57a8fe21397396d8f6422e0083a2af3b62a0d021
DDarkz <134795038+MrDDarkz@users.noreply.github.com>
2025-09-04 13:39:32 +0700

    Add files via upload

---
+++ bootstrap/bootstrap.js
@@ -0,0 +1,7 @@
+document.write(`
+    <header>
+        <div class="swap">
+            <a href="//red.ddarkz.xyz">DDarkz</a>
+        </div>
+    </header>
+`);
---
+++ bootstrap/head.js
@@ -0,0 +1,6 @@
+document.write(`
+    <link rel="stylesheet" href="//cdn.ddarkz.xyz/bootstrap.css">
+    <link rel="stylesheet" href="//cdn.ddarkz.xyz/SWAPthemes/theme-ddarkz.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="//cdn.ddarkz.xyz/SWAPusers/ddarkz/DDarkz.png">
+`)
